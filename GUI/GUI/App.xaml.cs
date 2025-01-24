d153030ef3399a49429cd511bafa40379d9e8bce
lusetto <ibraheem04a@gmail.com>
2025-01-24 15:10:16 +0100

    GUI foundation made

---
+++ GUI/GUI/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace GUI {
+    public partial class App : Application {
+        private void ExceptionHandler(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e) {
+            MessageBox.Show("An unhandled exception just occurred: " + e.Exception.Message, "Exception occured", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+        }
+    }
+
+}
